034b5612c6c9ba3b14a93d428a256c828c020f17
Guilherme Miranda <guilhermemiranda.ti@gmail.com>
2022-06-22 11:23:54 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [pokemons, setPokemons] = useState([]);
+	const [pokemonAtual, setPokemonAtual] = useState();
+
+	useEffect(() => {
+		buscarTodosPokemons();
+	}, []);
+
+	const buscarTodosPokemons = async () => {
+		const result = await axios.get("https://pokeapi.co/api/v2/pokemon");
+
+		setPokemons(result.data.results);
+	};
+
+	const buscarPokemon = async (url) => {
+		const result = await axios.get(url);
+
+		setPokemonAtual(result.data);
+	};
+
+	const buscarAnteriorPokemon = async () => {
+		const result = await axios.get(
+			"https://pokeapi.co/api/v2/pokemon/" + (pokemonAtual.id - 1)
+		);
+
+		setPokemonAtual(result.data);
+	};
+
+	const buscarProximoPokemon = async () => {
+		const result = await axios.get(
+			"https://pokeapi.co/api/v2/pokemon/" + (pokemonAtual.id + 1)
+		);
+
+		setPokemonAtual(result.data);
+	};
+
+	return (
+		<div>
+			<div className="btn-container">
+				{pokemons.map((item, index) => (
+					<button
+						onClick={() => buscarPokemon(item.url)}
+						className="btn-poke"
+						key={index}
+					>
+						{item.name}
+					</button>
+				))}
+			</div>
+
+			{pokemonAtual && (
+				<div className="container">
+					<button
+						onClick={() => buscarAnteriorPokemon()}
+						className="btn-poke"
+					>
+						Anterior
+					</button>
+					<div className="card-container">
+						<div className="card">
+							<img
+								src={pokemonAtual.sprites.front_default}
+								alt={pokemonAtual.name}
+							/>
+							<h1>{pokemonAtual.name}</h1>
+							<div className="tipo-container">
+								<h2>Tipo:</h2>
+								{pokemonAtual.types.map((item, index) => (
+									<span>| {item.type.name} | </span>
+								))}
+							</div>
+							<div className="tipo-container">
+								<h2>Habilidades:</h2>
+								{pokemonAtual.abilities.map((item, index) => (
+									<span key={index}>
+										| {item.ability.name} |{" "}
+									</span>
+								))}
+							</div>
+						</div>
+					</div>
+					<button
+						onClick={() => buscarProximoPokemon()}
+						className="btn-poke"
+					>
+						Próximo
+					</button>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
